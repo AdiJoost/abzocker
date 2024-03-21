b30f1a04db3a6f5e102f9f21039c855165b94200
AdiJoost <adi.joost@gmail.com>
2024-03-21 10:49:36 +0100

    bone constructed

---
+++ companySpliter.py
@@ -0,0 +1,22 @@
+
+DATA_FOLDER = "generatedData"
+
+def main():
+    #LOAD DATASET form datapath
+
+    #Filter dataset by company
+
+    #save Dataset
+    pass
+
+def loadDataset(path):
+    pass
+
+def filterSet(companyName):
+    pass
+
+def saveDataset(savingPath):
+    pass
+
+if __name__ == "__main__":
+    main()
