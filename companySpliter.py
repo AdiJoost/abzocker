e7d46e0a592bfbc17a622e81599f5938d355251e
AdiJoost <adi.joost@gmail.com>
2024-03-21 14:35:48 +0100

    refactoring

--- companySpliter.py
+++ companySpliter.py
@@ -3,44 +3,46 @@ import pandas as pd
 
 RESOURCE_FOLDER = "Resources"
 DATA_FOLDER = "generatedData"
+DATA_FILE = "sp500_stocks.csv"
 DATE_COLUMN = "Date"
 
-
 def main():
-    #get dataPath
-    dataPath = getDatapath("sp500_stocks.csv")
-    #LOAD DATASET form datapath
+    extractAndSaveCompanyStocks()
+
+def extractAndSaveCompanyStocks(companyName="MMM", saveFile="coolFile.csv", autoOverwrite=False):
+    """
+    companyName: String with the Symbol-Name of the company name you wish to extract
+    saveFile: name of the file, where the extracted dataFrame is saved. Example 'myFile.csv'
+    autoOverwrite: false -> the script asks for permission to overwrite an existing datafile. True -y The script just overwrites a file, if the saveFile already exists.
+    """
+    dataPath = getDatapath(DATA_FILE)
     df = loadDataset(dataPath)
-    #Filter dataset by company
-    df = filterSet("MMMfjh", df)
-    #save Dataset
-    saveDataset("coolFile.csv", df)
-    pass
+    df = filterSet(companyName, df)
+    saveDataset(saveFile, df, autoOverwrite)
 
-def getDatapath(filename):
+def getDatapath(filename, resourceFolder=RESOURCE_FOLDER):
     cwd = os.getcwd()
-    return os.path.join(cwd, RESOURCE_FOLDER, filename)
+    return os.path.join(cwd, resourceFolder, filename)
 
 
-def loadDataset(path):
+def loadDataset(path, dataColumn):
     df = pd.read_csv(path)
-    df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN])
+    df[dataColumn] = pd.to_datetime(df[dataColumn])
     df = df.sort_values(by=DATE_COLUMN, ascending=True)
     return df
 
 
 def filterSet(companyName, df, column="Symbol"):
-    #maybe do some validation if you have fun and time
     returnDf = df[df[column] == companyName]
     return returnDf
 
-def saveDataset(fileName, df):
-    path = getAndValidatePath(fileName)
+def saveDataset(fileName, df, autoOverwrite):
+    path = getAndValidatePath(fileName, autoOverwrite)
     df.to_csv(path)
 
-def getAndValidatePath(filename):
+def getAndValidatePath(filename, autoOverwrite):
     path = getSavingPath(filename)
-    if (os.path.exists(path)):
+    if (not autoOverwrite and os.path.exists(path)):
         if not askForOverwritePermission(filename):
             raise ("Saving Path already exists, abort")
     return path
