ea99c14ffef4447d4156612ab896ce37fb284f24
AdiJoost <adi.joost@gmail.com>
2024-03-21 11:30:12 +0100

    mayber do a refactoring

--- companySpliter.py
+++ companySpliter.py
@@ -1,30 +1,57 @@
 import os
+import pandas as pd
 
-DATA_FOLDER = "Resources"
+RESOURCE_FOLDER = "Resources"
+DATA_FOLDER = "generatedData"
+DATE_COLUMN = "Date"
+
 
 def main():
     #get dataPath
-    print(getDatapath("sp500_companies.csv"))
+    dataPath = getDatapath("sp500_stocks.csv")
     #LOAD DATASET form datapath
-    df = loadDataset()
+    df = loadDataset(dataPath)
     #Filter dataset by company
-
+    df = filterSet("MMMfjh", df)
     #save Dataset
+    saveDataset("coolFile.csv", df)
     pass
 
 def getDatapath(filename):
     cwd = os.getcwd()
-    return os.path.join(cwd, DATA_FOLDER, filename)
+    return os.path.join(cwd, RESOURCE_FOLDER, filename)
 
 
 def loadDataset(path):
-    pass
+    df = pd.read_csv(path)
+    df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN])
+    df = df.sort_values(by=DATE_COLUMN, ascending=True)
+    return df
 
-def filterSet(companyName):
-    pass
 
-def saveDataset(savingPath):
-    pass
+def filterSet(companyName, df, column="Symbol"):
+    #maybe do some validation if you have fun and time
+    returnDf = df[df[column] == companyName]
+    return returnDf
+
+def saveDataset(fileName, df):
+    path = getAndValidatePath(fileName)
+    df.to_csv(path)
+
+def getAndValidatePath(filename):
+    path = getSavingPath(filename)
+    if (os.path.exists(path)):
+        if not askForOverwritePermission(filename):
+            raise ("Saving Path already exists, abort")
+    return path
+
+def getSavingPath(filename):
+    cwd = os.getcwd()
+    return os.path.join(cwd, DATA_FOLDER, filename)
+
+def askForOverwritePermission(filename):
+    userInput = input(f"{filename} already exists, do you want to overwrite? [y/N]")
+    return userInput.lower() == "y"
 
 if __name__ == "__main__":
     main()
