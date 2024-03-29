879f326591aa51c05fdc624b0ddd251ab67d5c0a
danielschafi <daniel.schafi@bluewin.ch>
2024-03-29 21:06:56 +0100

    changed to pickleformat because its way faster

--- src/dataHandling/dataPreparation.py
+++ src/dataHandling/dataPreparation.py
@@ -3,12 +3,22 @@ import os
 from companySpliter import DATA_FOLDER, getStockSymbols
 
 def main():
-    prepareData()
+    stockSymbols = getStockSymbols()
+    prepareAllData(stockSymbols)
     
     
     
-def prepareData():
-    pass
+def prepareAllData(stockSymbols=["AAPL"]):
+    for stock in stockSymbols:     
+        filepath = getDataPath(stock)  
+        pd.read_pickle()
+        prepareDataFrame(stockName=stock)
+        
+        
+def getDataPath(stockName="AAPL"):
+    cwd = os.getcwd()
+    return os.path.join(cwd,DATA_FOLDER,stockName)
+    
 
 def prepareDataFrame(stockName="AAPL", DATA_FOLDER=""):
     pass
