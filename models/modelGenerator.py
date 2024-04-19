e469ddd4fa5a783e5747359e67640a7674cf8ae5
AdiJoost <adi.joost@gmail.com>
2024-04-19 15:52:49 +0200

    done

--- models/modelGenerator.py
+++ models/modelGenerator.py
@@ -1,15 +1,40 @@
 from keras.utils import timeseries_dataset_from_array
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+import numpy as np
 import os
+import pickle as pkl
 from targetTypeEnum import TargetType
 
 def main():
-    data =_loadData()
     numberOfDatapointsForPrediction = 10
+    X, Y = generateDataSet("coolFile.pkl", 20, TargetType.CLOSE, True)
+    print(X.shape)
+    print(Y)
     
-def generateDataSet(filename, sequenzLength, targetType:TargetType, saveAsFile=False):
+def generateDataSet(filename, sequenceLength, targetType:TargetType, saveAsFile=False, forceOverwrite=False):
     data = _loadData(filename)
+    X, Y = _getTimeWindow(data, sequenceLength, targetType)
+    if saveAsFile:
+        _saveFile(filename,X,Y,forceOverwrite)
+    return X, Y
+
+def _saveFile(filename, X, Y, forceOverwrite):
+    xPath = _getAndValidateSavePath(f"X_{filename[:-4]}.npy", forceOverwrite)
+    yPath = _getAndValidateSavePath(f"Y_{filename[:-4]}.npy", forceOverwrite)
+    with open(xPath, "wb") as file:
+        np.save(file, X)
+    with open(yPath, "wb") as file:
+        np.save(file, Y)
+
+def _getTimeWindow(data, sequenceLength, targetType):
+    generatedTimeseries = timeseries_dataset_from_array(data, targets=None, batch_size=None, sequence_length=sequenceLength + 1)
+    X = np.zeros((len(generatedTimeseries), sequenceLength, 7))
+    Y = np.zeros((len(generatedTimeseries)))
+    for i, batch in enumerate(generatedTimeseries):
+        X[i,:,:] = batch[:-1,:]
+        Y[i] = batch[-1,targetType.value[0]]
+    return X, Y
 
 def _loadData(filename):
     path = _getPath(filename)
@@ -23,6 +48,21 @@ def _getPath(filename):
     if len(head) < 1:
         raise NameError("Cannot get the working Path. Look at Source Code and Debug. :/")
     return os.path.join(head[0], "abzocker", "generatedData", filename)
+
+def _getAndValidateSavePath(filename, forceOverwrite):
+    path = _getSavePath(filename)
+    if not forceOverwrite and os.path.exists(path):
+        userInput = input(f"Flie {filename} already exists. Overwrite? [y/N]:")
+        if userInput != "y":
+            raise NameError("Abort saving")
+    return path
+
+def _getSavePath(filename):
+    cwd = os.getcwd()
+    head = cwd.split("abzocker")
+    if len(head) < 1:
+        raise NameError("Cannot get the working Path. Look at Source Code and Debug. :/")
+    return os.path.join(head[0], "abzocker", "generatedData", "timeSeries", filename)
 
 
 if __name__ == "__main__":
